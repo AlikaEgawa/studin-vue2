8bfb3f9cd646fdc26373ed02b127424087e5b902
AlikaEgawa <akiya9n@gmail.com>
2022-08-16 12:11:18 +0900

    2022/8/16

--- udemy-vuejs2/src/App.vue
+++ udemy-vuejs2/src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="main">
+        <button @click="myAnimation = 'slide'">Slide</button>
+        <button @click="myAnimation = 'fade'">Fade</button>
+        <p>{{myAnimation}}</p>
+
+        <button @click="show = !show">きりかえ</button>
+        <transition
+            enter-active-class="animate__animated animate__bounce"
+            leave-active-class="animate__animated animate__shakeX"
+            appear
+        >
+            <p v-if="show">hello</p>
+        </transition>
+        <transition
+            name="fade"
+            mode="out-in"
+        >
+            <p v-if="show" key="hi">こんにちは</p>
+            <p v-else key="gn">こんばんわ</p>
+        </transition>
+        <transition
+            :name="myAnimation"
+            appear
+        >
+            <div v-if="show" style="background-color: #f4f4f4;">
+                <p>bye</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+                <p>hello</p>
+            </div>
+        </transition>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    data() {
+        return {
+            show: true,
+            myAnimation: 'slide',
+        };
+    }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.fade-enter-from {
+    /* 現れる時の最初の状態 */
+    opacity: 0;
+}
+.fade-enter-active {
+    /* 現れる時のトランジション状態 */
+    transition: opacity 0.5s ease;
+}
+.fade-enter-to {
+    /* 消える時のトランジション状態 */
+    opacity: 1;
+}
+.fade-leave-from {
+    /* 消える時の最初の状態 */
+    opacity: 1;
+}
+.fade-leave-active {
+    /* 消える時のトランジションの状態 */
+    transition: opacity 0.5s ease;
+}
+.fade-leave-to {
+    /* 消える時の最後の状態 */
+    opacity: 0;
+}
+
+.slide-enter-from {
+    opacity: 0;
+}
+.slide-enter-active {
+    animation: slide-in 0.5s;
+    transition: opacity 0.5s ease;
+}
+.slide-enter-to {
+    opacity: 1;
+}
+.slide-leave-from {
+    opacity: 1;
+}
+.slide-leave-active {
+    animation: slide-in 0.5s reverse;
+    transition: opacity 1s ease;
+}
+.slide-leave-to {
+    opacity: 0;
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateX(100px)
+    }
+    to {
+        transform: translateX(0);
+    }
+}
+
+.main {
+    width: 70%;
+    margin: auto;
+    padding-top: 5rem;
+    text-align: center;
 }
 </style>
